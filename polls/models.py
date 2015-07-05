ad6057172721f5879cd248e87882681243b34b2c
coffeephantom <coffeephantom@users.noreply.github.com>
2015-07-06 00:25:17 +0800

    根据网站教程添加投票app的model，主要是问题以及选项两个类

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+#Example from django website
+
+class Question(models.Model):
+    """Question is somthing that needed to be polled."""
+    #define the area to show the question, the max length is 200
+    question_text = models.CharField(max_length=200)
+
+    #define the day that the question was published
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    """Choice is for people to selected"""
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+        
